a226c02d758d5d582eadff03e2e064f1e5b5d276
marcossilva11 <marcosant2170@gmail.com>
2025-08-30 12:23:00 -0300

    feat: permitir fechar o formulario quando clicar no botao cancelar dentro dele

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const btnAdicionarJogadora = document.querySelector("#btn-adicionar")
+const btnCancelar = document.querySelector("#form-cancelar")
 const secaoFormulario = document.querySelector("#form-section")
 
 btnAdicionarJogadora.addEventListener("click", () => {
   secaoFormulario.style.display = "flex"
+})
+
+btnCancelar.addEventListener("click", () => {
+  secaoFormulario.style.display = "none"
 })
