227d17c0753474c4033e9b24d0152b2af1315362
marcossilva11 <marcosant2170@gmail.com>
2025-08-30 12:45:05 -0300

    feat: iniciar o site e preparar as funcoes do crud

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const btnAdicionarJogadora = document.querySelector("#btn-adicionar")
-const btnCancelar = document.querySelector("#form-cancelar")
-const secaoFormulario = document.querySelector("#form-section")
+// Exibindo formulario
+const btnAdicionarJogadora = document.querySelector("#btn-adicionar");
+const btnCancelar = document.querySelector("#form-cancelar");
+const secaoFormulario = document.querySelector("#form-section");
 
 btnAdicionarJogadora.addEventListener("click", () => {
-  secaoFormulario.style.display = "flex"
-})
+  secaoFormulario.style.display = "flex";
+});
 
 btnCancelar.addEventListener("click", () => {
-  secaoFormulario.style.display = "none"
-})
+  secaoFormulario.style.display = "none";
+});
+
+// Inicializacao
+window.onload = function () {
+  exibirJogadoras();
+};
+
+// CREATE
+function adicionarJogadora() {}
+
+// READ
+function exibirJogadoras() {}
+
+// UPDATE
+function atualizarJogadora() {}
+
+// DELETE
+function deletarJogadora() {}
