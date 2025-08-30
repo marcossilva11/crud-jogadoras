baa4014d064b1495443ec07f7991ff4015517810
marcossilva11 <marcosant2170@gmail.com>
2025-08-30 12:22:16 -0300

    feat: adicionar funcionalidade de exibir o formulario quando clicar no botao para adicionar jogadora

---
+++ app.js
@@ -0,0 +1,6 @@
+const btnAdicionarJogadora = document.querySelector("#btn-adicionar")
+const secaoFormulario = document.querySelector("#form-section")
+
+btnAdicionarJogadora.addEventListener("click", () => {
+  secaoFormulario.style.display = "flex"
+})
